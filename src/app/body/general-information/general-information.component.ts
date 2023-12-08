d4b81cd2a05f0f6a38d83a9f34ab459192944455
Luan Charlys <firminocharlys@gmail.com>
2023-12-08 14:43:44 -0300

    feat(home): add docs components for my page

---
+++ src/app/body/general-information/general-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-information',
+  standalone: true,
+  imports: [],
+  template: ` <p>general-information works!</p> `,
+  styleUrl: './general-information.component.scss',
+})
+export class GeneralInformationComponent {}
---
+++ src/app/body/my-projects/my-projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-projects',
+  standalone: true,
+  imports: [],
+  template: ` <p>my-projects works!</p> `,
+  styleUrl: './my-projects.component.scss',
+})
+export class MyProjectsComponent {}
